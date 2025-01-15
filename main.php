dd39eae40322f7ae0cd65e67c7d51dc057f3506d
= <=>
2025-01-15 18:28:03 -0300

    1.1.0 test release

--- main.php
+++ main.php
@@ -3,16 +3,16 @@
  * Asas Virtuais WP
  *
  * Plugin Name: Asas Virtuais WP
- * Version: 1.0.8
+ * Version: 1.1.0
  */
 
 try {
     // Require the Composer autoloader.
     require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
     \YahnisElsts\PluginUpdateChecker\v5\PucFactory::buildUpdateChecker(
-        'https://raw.githubusercontent.com/asas-virtuais/asasvirtuais-wp/main/metadata.json',
-        __DIR__ . '/asasvirtuais-wp.php',
-        'asasvirtuais-wp'
+        'https://raw.githubusercontent.com/asasvirtuais/wordpress-plugin/main/metadata.json',
+        __DIR__ . '/main.php',
+        'wordpress-plugin'
     );
     // Your existing code to require the main plugin file.
     require_once plugin_dir_path(__FILE__) . 'src/main.php';
